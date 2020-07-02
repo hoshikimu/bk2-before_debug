fe1d1bc1dff6d882f14d618946651459a93cd811
vagrant <vagrant@localhost.localdomain>
2020-07-02 06:07:11 +0000

    finish bk2-before_debug

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_11_083403) do
+ActiveRecord::Schema.define(version: 2020_07_02_052827) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
